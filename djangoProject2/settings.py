662d4f61f1ba941bf2077556cf68063096aaab6d
yurasblv <yurasblv.y@gmail.com>
2021-11-29 22:02:31 +0200

    heroku deploy

--- djangoProject2/settings.py
+++ djangoProject2/settings.py
@@ -36,6 +36,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    'django_heroku',
     "newsapi",
     "rest_framework",
 ]
@@ -117,9 +118,6 @@ USE_TZ = True
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
-)
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
